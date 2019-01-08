cc2208d75c3b8815df83b479fd84606abaa485a9
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2019-01-08 11:50:40 +0100

    ver.1.2.2

---
+++ src/main/java/pl/javastart/exampleproject/SecurityConfig.java
@@ -0,0 +1,20 @@
+package pl.javastart.exampleproject;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .loginProcessingUrl("/processlogin")
+                .permitAll();
+    }
+}
